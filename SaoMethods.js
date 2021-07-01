e87e7d6f2e25ccbe1f5b26fb4c7b21c7c5fd3457
Ignacio Parszyk <iparszyk@kalenislims.com>
2021-07-01 10:45:00 -0300

    Improve HTML widget refresh

--- SaoMethods.js
+++ SaoMethods.js
@@ -1396,7 +1396,16 @@ Sao.View.Form.HTML.prototype.init = function (view, attributes) {
 
 Sao.View.Form.HTML.prototype.display = function () {
     Sao.View.Form.HTML._super.display.call(this);
-
+    var value = "";
+    if(this.field && this.field.description.name === 'diagnosis'){
+        
+        if(this.record){
+            value = this.record._values[this.field.description.name];
+            
+        }
+    }
+    
+    
 
     Sao.KalenisAddons.Components.createHtmlField({
         element: this.el[0], sao_props: {
@@ -1404,6 +1413,7 @@ Sao.View.Form.HTML.prototype.display = function () {
             record: this.record,
             attributes: this.attributes,
             screen: this.view.screen,
+            value:value
         }
     });
 
